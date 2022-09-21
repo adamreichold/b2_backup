bb8748c0cabad2f86a6818cbcd8f900aa8e0ea72
Adam Reichold <adam.reichold@t-online.de>
2022-09-21 10:46:01 +0200

    Add crossbuild infrastructure to easily produce arm64 builds.

---
+++ crossbuild.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -xe
+
+: "${ARCH:=arm64}"
+: "${TARGET:=aarch64-unknown-linux-gnu}"
+: "${TARGET_CC:=aarch64-linux-gnu-gcc}"
+
+podman run --interactive --rm --volume $PWD:/src rust:bullseye /bin/bash -xe <<EOF
+
+export DEBIAN_FRONTEND="noninteractive"
+
+apt-get update
+apt-get install --yes --no-install-recommends crossbuild-essential-${ARCH} libclang-dev
+
+export CARGO_TARGET_DIR="target-crossbuild-${ARCH}"
+export RUSTFLAGS="-C linker=${TARGET_CC}"
+export TARGET_CC="${TARGET_CC}"
+
+cd /src
+rustup target add ${TARGET}
+cargo build --no-default-features --release --target=${TARGET}
+
+EOF
